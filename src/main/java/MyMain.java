8c32f7257746fb894e0d3316f823e79ba19adcf7
brunodovalcallao <24bdovalcallao@athenian.org>
2021-09-23 20:06:55 -0700

    sebastion helped a brotha out a bit

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,14 +2,32 @@ public class MyMain {
 
     // Reverses an array
     public static int[] reverse(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int[] newarray = new int[arr.length];
+        int counter = 0;
+        for (int i = arr.length-1; i>=0; i--){
+            System.out.println(i);
+            newarray[counter] = arr[i];
+            counter ++;
+
+        }
+        return newarray;
     }
 
     // Finds the second largest number in an array
     public static int secondLargest(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        int greatest = -10000;
+        for (int i = 0; i <= arr.length-1; i++) {
+            if (arr[i] > greatest) {
+                greatest = arr[i];
+            }
+        }
+        int newgreatest = -100000;
+        for (int i = 0; i <= arr.length-1; i++) {
+            if (arr[i] > newgreatest && arr[i] != greatest) {
+                newgreatest = arr[i];
+            }
+        }
+        return newgreatest;
     }
 
     // This method is given a word, and it checks to see
@@ -22,7 +40,11 @@ public class MyMain {
         // This is our dictionary of valid words
         String[] dictionary = {"the","of","and","a","to","in","is","you","that","it","he","was","for","on","are","as","with","his","they","I","at","be","this","have","from","or","one","had","by","word","but","not","what","all","were","we","when","your","can","said","there","use","an","each","which","she","do","how","their","if","will","up","other","about","out","many","then","them","these","so","some","her","would","make","like","him","into","time","has","look","two","more","write","go","see","number","no","way","could","people","my","than","first","water","been","call","who","oil","its","now","find","long","down","day","did","get","come","made","may","cat","dog","cats","dogs"};
 
-        // REPLACE WITH YOUR CODE HERE
+        for (int i = 0; i< dictionary.length; i++){
+            if (word.equals(dictionary[i])){
+                return true;
+            }
+        }
         return false;
     }
 
